1ce1418bae81e6f0511d838c6b79179361ad18a8
Muskan Swarup <muskanswarup680@gmail.com>
2024-07-04 01:04:46 +0530

    get all tweets done

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -1,4 +1,5 @@
 import {Tweet} from '../models/tweetSchema.js';
+import { User } from '../models/userSchema.js';
 
 export const createTweet = async (req, res) => {
     try{
@@ -70,5 +71,29 @@ export const likeOrDislike = async(req , res) => {
     }catch(error){
         console.log(error);
     }
+}
+
+export const getAllTweets = async(req, res) => {
+    // loggedInUSer tweet + following's tweets
+    try{
+        const id = req.params.id;
+        // const userId = req.body.id;
+        // const userId = req.params.id;
+        const loggedInUser = await User.findById(id);
+        const loggedInUserTweets = await Tweet.find({userId: id});
+        console.log("logged in's tweets : " + loggedInUserTweets);
+        const followingUserTweets = await Promise.all(loggedInUser.following.map((otherUsersId) => {
+            return Tweet.find({userId: otherUsersId});
+        }));
+        
+        return res.status(200).json({
+            tweets: loggedInUserTweets.concat(...followingUserTweets),
+            success: true
+        })
+    }catch(error){
+        console.log(error);
+    }
+
+   
 }
 
--- backend/models/tweetSchema.js
+++ backend/models/tweetSchema.js
@@ -12,8 +12,12 @@ const tweetSchema = mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        extended: true
-    }
+        // extended: true
+    },
+    userDetails:{
+        type:Array,
+        default:[]
+    },
     
 } , {timestamps : true});
 
