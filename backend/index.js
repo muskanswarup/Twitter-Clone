72eaee735ab5bbc7b8104437b8032b3f76654831
Muskan Swarup <muskanswarup680@gmail.com>
2024-07-03 19:36:26 +0530

    create register and login routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import databaseConnection from './config/database.js';
+import cookieParser from 'cookie-parser';
+import userRoute from './routes/userRoute.js';
 
 dotenv.config({
     path: ".env"
@@ -9,7 +11,23 @@ dotenv.config({
 
 databaseConnection();
 const app = express();
-const PORT = 8080;
+
+// Middlewares
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+// API
+app.use("/api/v1/user" , userRoute);
+
+app.get("/home" , (req , res) => {
+    res.status(200).json({
+        message: "coming from backend..."
+    })
+} )
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening at port ${process.env.PORT}`);
---
+++ backend/models/tweetSchema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const tweetSchema = mongoose.Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    like: {
+        type: Array,
+        default: []
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        extended: true
+    },
+    bookmarks:{
+        type: Array,
+        default: []
+    }
+} , {timestamps : true});
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
